5e4cb01a2dbb77412edc32119b05ad3aa75b0358
creativeeyestudio <contact@creative-eye.fr>
2025-05-03 11:04:17 +0200

    Accordion system

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,33 +1,28 @@
-import { IonAccordion, IonAccordionGroup, IonItem, IonLabel } from "@ionic/react"
-import { arrowDownOutline } from "ionicons/icons"
+import { IonAccordion, IonAccordionGroup, IonAvatar, IonIcon, IonItem, IonLabel } from "@ionic/react"
+import { arrowDownOutline, bedOutline } from "ionicons/icons"
 
 const Services = () => {
     return(
         <section>
             <h1>Nos services</h1>
             <IonAccordionGroup>
-
                 <IonAccordion value="first" toggleIcon={arrowDownOutline} toggleIconSlot="end">
-                    <IonItem slot="header">
-                        <IonLabel>Nom du service</IonLabel>
+                    <IonItem slot="header" color="light">
+                        <IonAvatar>
+                            <IonIcon icon={bedOutline} size="large"></IonIcon>
+                        </IonAvatar>
+                        <IonLabel class="ion-padding-start">Nom du service</IonLabel>
                     </IonItem>
                     <div className="ion-padding" slot="content">
                         <p>Contenu du service</p>
                     </div>
                 </IonAccordion>
-
                 <IonAccordion value="second" toggleIcon={arrowDownOutline} toggleIconSlot="end">
-                    <IonItem slot="header">
-                        <IonLabel>Nom du service</IonLabel>
-                    </IonItem>
-                    <div className="ion-padding" slot="content">
-                        <p>Contenu du service</p>
-                    </div>
-                </IonAccordion>
-                
-                <IonAccordion value="third" toggleIcon={arrowDownOutline} toggleIconSlot="end">
-                    <IonItem slot="header">
-                        <IonLabel>Nom du service</IonLabel>
+                    <IonItem slot="header" color="light">
+                        <IonAvatar>
+                            <IonIcon icon={bedOutline} size="large"></IonIcon>
+                        </IonAvatar>
+                        <IonLabel class="ion-padding-start">Nom du service</IonLabel>
                     </IonItem>
                     <div className="ion-padding" slot="content">
                         <p>Contenu du service</p>
