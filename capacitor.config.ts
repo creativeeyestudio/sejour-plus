0db2d907b9c84a26ef9bbdac5c535ebeb287628d
creativeeyestudio <contact@creative-eye.fr>
2025-12-26 16:06:16 +0000

    Remove React Project

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sejour-plus',
-  webDir: 'dist'
-};
-
-export default config;
